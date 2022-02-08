daa16f970b98ee755084bfda23f6e18809d51433
LarisaZaripova <larik7lolik@gmail.com>
2022-02-08 11:44:09 +0300

    add Example 19

--- Program.cs
+++ Program.cs
@@ -1,2 +1,25 @@
 ﻿// Задача 19. Определить номер четверти плоскости, в которой находится точка с координатами X и Y, причём X не равен 0, Y не равен 0.
-
+Console.WriteLine("Введите x");
+int x = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите y");
+int y = int.Parse(Console.ReadLine());
+if (x == 0 || y == 0)
+{
+    Console.WriteLine("Нет четверти");
+}
+else if  (x > 0 && y > 0)
+{
+   Console.WriteLine("Первая четверть"); 
+}
+else if(x > 0 && y < 0)
+{
+    Console.WriteLine("Вторая четверть");
+}
+else if(x < 0 && y < 0)
+{
+    Console.WriteLine("Третья четверть");
+}
+else if(x < 0 && y > 0)
+{
+    Console.WriteLine("Четвертая четверть");
+}
